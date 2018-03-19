af8a9a3eab5c35a71f10c2a06b86a624d8244cc6
bowen0701 <bowen0701@gmail.com>
2018-03-19 20:39:18 +0800

    Rename run_jupyter.sh to run_cmd.sh and launch TensorBoard at port 6006 on localhost

---
+++ run_cmd.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+jupyter notebook --no-browser --allow-root --port 8888 --ip 0.0.0.0 --NotebookApp.token='' &
+tensorboard --port 6006 --ip 0.0.0.0 --logdir /logs
--- run_jupyter.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-jupyter notebook --no-browser --allow-root --port 8888 --ip 0.0.0.0 --NotebookApp.token=''
