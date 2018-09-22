ffd809699bbb35aab00dd60c9936a8d610d0435c
bowen0701 <bowen0701@gmail.com>
2018-09-22 21:15:39 +0200

    Comment out tensorboard now

--- run_cmd.sh
+++ run_cmd.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-jupyter notebook --no-browser --allow-root --port 8888 --ip 0.0.0.0 --NotebookApp.token='' &
-tensorboard --port 6006 --ip 0.0.0.0 --logdir /logs
+# jupyter notebook --no-browser --allow-root --port 8888 --ip 0.0.0.0 --NotebookApp.token='' &
+# tensorboard --port 6006 --ip 0.0.0.0 --logdir /logs
+jupyter notebook --no-browser --allow-root --port 8888 --ip 0.0.0.0 --NotebookApp.token=''
